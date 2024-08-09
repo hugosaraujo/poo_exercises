9e0ad886f16ee4aee5fd51d9a53f99dd020652c7
Hugo de Araujo <hugo.saraujo@tutanota.com>
2024-08-08 21:04:04 -0300

    Adicionando enunciado dos exercícios

--- models/song.py
+++ models/song.py
@@ -3,3 +3,17 @@ class song:
     artist = ''
     running_time = 0
 
+backbone = song()
+backbone.title = 'Backbone'
+backbone.artist = 'Gojira'
+backbone.running_time = 259
+anklebitters = song()
+anklebitters.title = 'Anklebiters'
+anklebitters.artist = 'Paramore'
+anklebitters.running_time = 168
+only_exception = song()
+only_exception.title = 'Only Exception'
+only_exception.artist = 'Paramore'
+only_exception.running_time = 275
+
+print(backbone.title)
